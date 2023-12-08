2346e87a2e144a563ecd468ec81560a558cdd17b
InviBull <virtualclass.raghav@gmail.com>
2023-12-08 23:20:20 +0530

    Day 09: Preparations

--- aoc_io.py
+++ aoc_io.py
@@ -1,15 +1,13 @@
-import os
-import time
+import os, time
 
-def calculate_time_taken(function):
-  """Calculates the time taken to run a function."""
+def stopwatch(function):
 
   def wrapper(*args, **kwargs):
-    start_time = time.perf_counter()  # High-precision counter
+    start_time = time.perf_counter()
     result = function(*args, **kwargs)
     end_time = time.perf_counter()
     time_taken = end_time - start_time
-    print(f"Function '{function.__name__}' took {time_taken:.4f} seconds to run.")
+    print(f"{function.__name__}' took {time_taken:.4f} seconds to run.")
     return result
 
   return wrapper
@@ -26,13 +24,5 @@ def fetch(day):
 def run(solution):
     sol = solution()
     
-    @calculate_time_taken
-    def p1():
-        print("Part 1: ", sol.calculate_p1())
-
-    @calculate_time_taken
-    def p2():
-        print("Part 2: ", sol.calculate_p2())
-    
-    p1()
-    p2()
+    print("Part 1: ", sol.calculate_p1())
+    print("Part 2: ", sol.calculate_p2())
---
+++ day_09.py
@@ -0,0 +1,24 @@
+from aoc_io import fetch, run, stopwatch
+import math, re
+
+class Solution:
+    def __init__(self):
+        self.data = self._parse_data()
+        self.length = len(self.data)
+
+    def _parse_data(self):
+        data = fetch("09", "2023").splitlines()
+        return data
+
+    @stopwatch
+    def calculate_p1(self) -> int:
+        p1 = 0
+        return p1
+
+    @stopwatch
+    def calculate_p2(self) -> int:
+        p2 = 0
+        return p2
+
+if __name__ == "__main__":
+    run(Solution)
